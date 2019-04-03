51040cf1a68b72edfd1a168423729d6ae3cca52b
Matthias Rosenstock <matthias@berla.net>
2019-04-03 08:54:17 +0200

    Strip debug information from binary.

--- .github/build.sh
+++ .github/build.sh
@@ -9,7 +9,7 @@ echo "0.1.0" > .version
 for GOOS in darwin linux windows; do
 	SUFFIX=`[ $GOOS = "windows" ] && echo ".exe"`
 	for GOARCH in 386 amd64; do
-		for D in swivelp swivelt; do go build -v -o .release/$GOOS/$GOARCH/$D$SUFFIX cmd/$D/main.go; done
+		for D in swivelp swivelt; do go build -ldflags="-s -w" -v -o .release/$GOOS/$GOARCH/$D$SUFFIX cmd/$D/main.go; done
 		tar --transform 's/.*\///g' -czvf .dist/swivel-authenticator-$GOOS-$GOARCH.tar.gz .release/$GOOS/$GOARCH/* README.md LICENSE
 	done
 done
